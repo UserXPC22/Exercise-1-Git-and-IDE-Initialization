972f6594000a105bcac6fcb515cfaf5fb1558fa8
UserXPC22 <cabasanxp@gmail.com>
2024-08-31 22:03:06 +0800

    Add personalized greeting feature

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+var userName = prompt("Please enter your name:");
+
+if (userName) {
+    console.log('Hello, ' + userName + '!');
+} else {
+    console.log('Hello, World!');
+}
